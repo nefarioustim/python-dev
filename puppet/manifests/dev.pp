77b4c7f04be4ecaf65678ae9838791b508456ec9
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-06-05 09:31:06 +0100

    Add Puppet provisioning files.

---
+++ puppet/manifests/dev.pp
@@ -0,0 +1,22 @@
+$user = 'vagrant'
+$project = 'python-dev'
+
+Exec {
+    path => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin'
+}
+
+include core-env
+
+class { "user":
+    projectpath =>  "/home/${user}/${project}",
+    username => $user,
+    groupname => $user
+}
+
+include libxml
+
+include python
+include python::virtualenv
+include python::supervisor
+
+include redis
